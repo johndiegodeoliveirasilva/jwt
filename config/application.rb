1108590599a6d19f834846abfc5e47f5865efa3a
John Diego <johnoliveirasilva9@gmail.com>
2023-04-04 22:18:15 -0300

    Create user Model

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Jwt
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.eager_load_paths += config.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
