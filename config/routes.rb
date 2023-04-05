0e23c9a267eb0ae553e79154a04c55b278a74650
John Diego <johnoliveirasilva9@gmail.com>
2023-04-04 22:00:05 -0300

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+    end
+  end
 end
