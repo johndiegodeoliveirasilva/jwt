64d5b13207b8e6563fcdc52f9c84ae967d2d4f82
John Diego <johnoliveirasilva9@gmail.com>
2023-04-14 00:48:40 -0300

    setup bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_secure_password
+
   validates :email, uniqueness: true
   validates :email, format: { with: /@/ }
   validates :password_digest, presence: true
