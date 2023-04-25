8f3e44fb2eae5cb2fbaa5a10da783294490ad16d
John Diego <johnoliveirasilva9@gmail.com>
2023-04-25 00:13:08 -0300

    refactore test controller user

--- spec/controller/users_controller_rspec.rb
+++ spec/controller/users_controller_rspec.rb
@@ -1,5 +1,3 @@
-# users_controller_spec.rb
-
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
@@ -9,14 +7,11 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
   describe 'POST #create' do
     context 'with valid attributes' do
-      it 'create a new user' do
+      it 'creates a new user' do
         expect do
           post :create, params: { user: valid_attributes }
-        end.to change(User, :count).by(1)
-      end
+        end.to change { User.count }.by(1)
 
-      it 'response created 200' do
-        post :create, params: { user: valid_attributes }
         expect(response).to have_http_status(:created)
       end
     end
@@ -24,86 +19,63 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     context 'with invalid attributes' do
       it 'does not create a new user' do
         expect do
-          post :create, params: { user: invalid_attributes }
-        end.to change(User, :count).by(0)
-      end
+          post :create, params: { user: invalid_attributes } 
+        end.to change { User.count }.by(0)
 
-      it 'return a error unprocessable_entity' do
-        post :create, params: { user: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 
   describe 'DELETE #destroy' do
-    before do
-      user
-    end
+    before { user }
 
-    context 'should destroy user' do
-      it 'delete the user' do
-        expect do
-          delete :destroy, params: { id: user }
-        end.to change(User, :count).by(-1)
-      end
-
-      it 'return respond no content' do
-        delete :destroy, params: { id: user }
-        expect(response).to have_http_status(:no_content)
-      end
+    it 'destroys the user' do
+      expect { delete :destroy, params: { id: user } }
+        .to change { User.count }.by(-1)
+      expect(response).to have_http_status(:no_content)
     end
   end
 
-  describe 'update #PATH' do
-    let(:new_attributes) do
-      { email: Faker::Internet.email }
-    end
+  describe 'PATCH #update' do
+    let(:new_attributes) { { email: Faker::Internet.email } }
+    let(:new_invalid_attributes) { { email: 'testegmail.com' } }
 
-    let(:new_invalid_attributes) do
-      { email: 'testegmail.com' }
-    end
-
-    context 'should update user' do
-      before do
-        patch :update, params: { id: user, user: new_attributes }
-      end
+    context 'with valid attributes' do
+      before { patch :update, params: { id: user, user: new_attributes } }
 
       it 'updates the user' do
         user.reload
         expect(user.email).to eq(new_attributes.fetch(:email))
       end
 
-      it 'return success on update user' do
+      it 'returns success' do
         expect(response).to have_http_status(:success)
       end
     end
 
-    context 'should not update user' do
-      before do
-        patch :update, params: { id: user, user: new_invalid_attributes }
-      end
+    context 'with invalid attributes' do
+      before { patch :update, params: { id: user, user: new_invalid_attributes } }
 
-      it 'dont updates the user' do
+      it 'does not update the user' do
         user.reload
         expect(user.email).not_to eq(new_invalid_attributes.fetch(:email))
       end
 
-      it 'return unprocessable_entity on update user' do
+      it 'returns unprocessable_entity' do
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 
-  describe 'show #GET' do
+  describe 'GET #show' do
     let(:user) { create(:user) }
 
-    context 'should show user' do
-      it 'return user json' do
-        get :show, params: { id: user }
+    it 'returns the user in JSON format' do
+      get :show, params: { id: user }
 
-        json_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user.email).to eq(json_response.dig(:email))
-      end
+      json_response = JSON.parse(response.body, symbolize_names: true)
+      expect(user.email).to eq(json_response.dig(:email))
     end
   end
 end
